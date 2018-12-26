3a66c5d26cca60fb37f85a47f6f33805f2621b96
Ernest Guevarra <ernest@validmeasures.org>
2018-12-26 07:08:57 +0000

    create inst/

---
+++ inst/createHexSticker.R
@@ -0,0 +1,6 @@
+library(hexSticker)
+
+sticker(subplot = "inst/figures/oipa.png", s_x = 1, s_y = 0.9, s_width = 0.5,
+        package = "oipa", p_color = "#CC3333", p_y = 1.6,
+        h_size = 1, h_color = "#CC3333", h_fill = NA,
+        filename = "man/figures/oipa.png")
