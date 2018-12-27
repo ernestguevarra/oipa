084bce1bee13fdce3575e4265bb76604859d1416
Ernest Guevarra <ernest@validmeasures.org>
2018-12-27 01:12:06 +0000

    update get_countries.R

---
+++ R/09-get_countries.R
@@ -0,0 +1,52 @@
+################################################################################
+#
+#' get_countries
+#'
+#' Function to get activities endpoint from IATI-compliant datasets.
+#'
+#' @param url URL for websites containing IATI-compliant datasets.
+#' @param query Search/query parameter. For IATI activites, this can be
+#'     specified as `country_id`.
+#' @param value Value to pass to specified `query`.
+#'
+#' @return Parsed content on countries from IATI-compliant datasets.
+#'
+#' @examples
+#' get_countries(url = "https://devtracker.dfid.gov.uk",
+#'               query = "country_id",
+#'               value = "AC")
+#'
+#' @export
+#'
+#
+################################################################################
+
+get_countries <- function(url, query, value) {
+  url <- paste(httr::modify_url(url = url,
+                                path = "/api/countries/",
+                                query = query),
+               paste("=", value, sep = ""), sep = "")
+
+  resp <- httr::GET(url)
+
+  if (httr::http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  parsed <- jsonlite::fromJSON(httr::content(x = resp,
+                                             as = "text",
+                                             encoding = "UTF-8"),
+                               simplifyVector = FALSE)
+
+  structure(
+    list(
+      content = parsed,
+      path = "/api/countries/",
+      response = resp
+    ),
+    class = "oipa_api"
+  )
+}
+
+
+
