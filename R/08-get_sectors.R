8383740b72c6cdf1fc16a314c201a98401025768
Ernest Guevarra <ernest@validmeasures.org>
2018-12-27 00:47:36 +0000

    create get_sectors.R

---
+++ R/08-get_sectors.R
@@ -0,0 +1,52 @@
+################################################################################
+#
+#' get_sectors
+#'
+#' Function to get sectors endpoint from IATI-compliant datasets.
+#'
+#' @param url URL for websites containing IATI-compliant datasets.
+#' @param query Search/query parameter. For IATI activites, this can be
+#'     specified as `sector_id`.
+#' @param value Value to pass to specified `query`.
+#'
+#' @return Parsed content on sectors from IATI-compliant datasets.
+#'
+#' @examples
+#' get_sectors(url = "https://devtracker.dfid.gov.uk",
+#'             query = "sector_id",
+#'             value = "11110")
+#'
+#' @export
+#'
+#
+################################################################################
+
+get_sectors <- function(url, query, value) {
+  url <- paste(httr::modify_url(url = url,
+                                path = "/api/sectors/",
+                                query = query),
+               paste("=", value, sep = ""), sep = "")
+
+  resp <- httr::GET(url)
+
+  if (httr::http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  parsed <- jsonlite::fromJSON(httr::content(x = resp,
+                                             as = "text",
+                                             encoding = "UTF-8"),
+                               simplifyVector = FALSE)
+
+  structure(
+    list(
+      content = parsed,
+      path = "/api/sectors/",
+      response = resp
+    ),
+    class = "oipa_api"
+  )
+}
+
+
+
